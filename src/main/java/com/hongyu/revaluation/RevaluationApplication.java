df292db58c4b03671f153c899d873b182e7c33eb
JaikenWong <Jiccence@gmail.com>
2024-01-15 11:20:49 +0800

    初始化创建用户管理接口

---
+++ src/main/java/com/hongyu/revaluation/RevaluationApplication.java
@@ -0,0 +1,13 @@
+package com.hongyu.revaluation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevaluationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RevaluationApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/hongyu/revaluation/api/UserManagementApi.java
@@ -0,0 +1,13 @@
+package com.hongyu.revaluation.api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author JaikenWong
+ * @since 2024-01-15 11:19
+ **/
+@RestController
+public class UserManagementApi {
+
+
+}
