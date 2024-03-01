8efd10f60a48d34f444912e767de5e1738859d64
JaikenWong <Jiccence@gmail.com>
2024-03-01 14:20:03 +0800

    重命名配置文件

--- src/main/java/com/hongyu/revaluation/shiro/CustomShiroConfig.java
+++ src/main/java/com/hongyu/revaluation/shiro/CustomShiroConfig.java
@@ -19,7 +19,7 @@ import org.springframework.context.annotation.Configuration;
  * @since 2024-02-29 22:36
  **/
 @Configuration
-public class ShiroConfig {
+public class CustomShiroConfig {
 
 
     /**
